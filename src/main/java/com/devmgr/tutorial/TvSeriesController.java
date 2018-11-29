bab1d9ba50cdd986e2ebdeaf7c48bdd922b4a675
Small Rui <603049583@qq.com>
2018-11-29 23:09:56 +0800

    一个简单的RESTful API

--- src/main/java/com/devmgr/tutorial/TvSeriesController.java
+++ src/main/java/com/devmgr/tutorial/TvSeriesController.java
@@ -4,21 +4,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/tvseries")
 public class TvSeriesController {
     @GetMapping
-    public Map<String, Object> sayHello() {
-        Map<String, Object> result = new HashMap<>();
-        HashMap map1 = new HashMap();
-        map1.put("name", "c80d1b66e49667c4f33ed319d00c7194");
-        map1.put("age", 18);
-        map1.put("sex", 1);
-        map1.put("token", "5bab36d9959d69031c86b96c");
-        result.putAll(map1);
-        return result;
+//    public Map<String, Object> sayHello() {
+//        Map<String, Object> result = new HashMap<>();
+//        HashMap map1 = new HashMap();
+//        map1.put("name", "c80d1b66e49667c4f33ed319d00c7194");
+//        map1.put("age", 18);
+//        map1.put("sex", 1);
+//        map1.put("token", "5bab36d9959d69031c86b96c");
+//        result.putAll(map1);
+//        return result;
+//    }
+//    @GetMapping
+    public List<TvSeriesDto> getAll(){
+        List<TvSeriesDto> list = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2016,Calendar.OCTOBER,2,0,0);
+        list.add(new TvSeriesDto(1,"WestWorld",1,calendar.getTime()));
+        return list;
     }
 }
